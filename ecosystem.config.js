34a86c68c3c5f552e5e3c6f6fdd530c693a5e4f9
dnialwill <dnialwill@gmail.com>
2018-11-13 01:34:54 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+        name: 'tempollo_api',
+        script: 'npm',
+        args: 'start'
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-18-204-203-52.compute-1.amazonaws.com',
+            key: '~/.ssh/administrator-key-pair-useast1.pem',
+            ref: 'origin/master',
+            repo: 'git@github.com:dnialwill/tempollo_api.git',
+            path: '/home/ubuntu/tempollo_api',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+};
