e7c4f1ca88abac360d886676622e6e2a044ac873
dnialwill <dnialwill@gmail.com>
2018-11-13 23:38:58 -0500

    Renaming app.ts to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 
-const app = new Koa();
+const server = new Koa();
 const router = new Router();
 
 router.get('/*', async (ctx) => {
     ctx.body = 'Hello World!';
 });
 
-app.use(router.routes());
+server.use(router.routes());
 
-app.listen(3000);
+server.listen(3000);
 
 console.log('Server running on port 3000', process.env.TEST);
